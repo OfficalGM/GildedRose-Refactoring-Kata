0b5e24c8714f277cb670ce208d72176af2387f98
Tony <gm10285@gmail.com>
2020-09-28 20:43:32 +0800

    refactor >0

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,16 +7,19 @@ class GildedRose {
         this.items = items;
     }
 
-    boolean isEqualToItemName(Item item, String name) {
+    boolean isEqualToItemName(final Item item, final String name) {
         return item.name.equalsIgnoreCase(name);
+    }
+
+    boolean isGreaterThanZero(Item item) {
+        return item.quality > 0;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
             if (!isEqualToItemName(items[i], "Aged Brie")
                     && !isEqualToItemName(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
+                if (isGreaterThanZero(items[i])) {
                     if (!isEqualToItemName(items[i], "Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
@@ -48,7 +51,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!isEqualToItemName(items[i], "Aged Brie")) {
                     if (!isEqualToItemName(items[i], "Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
+                        if (isGreaterThanZero(items[i])) {
                             if (!isEqualToItemName(items[i], "Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
